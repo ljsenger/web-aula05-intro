4d51993035b3be87dcb3c433fc357bebd0789225
Luciano Senger <ljsenger@gmail.com>
2019-10-15 16:35:38 -0300

    trabalhando com rotas

--- app.py
+++ app.py
@@ -7,4 +7,9 @@ app = Flask("app")
 @app.route("/")
 def hello():
     return "<h1> Ola Mundo, <strong> estou aprendendo o framework Flask </strong> </h1> ", 200
+
+@app.route("/uepg")
+@app.route("/deinfo")
+def uepg():
+    return "<h3> Ola UEPG </h3>"
     
